8b4b03a9f898cc41b6a3651833a8ea40e4659932
zenyuca <zenyuca@163.com>
2017-08-25 23:31:33 +0800

    多入口

--- src/index.js
+++ src/index.js
@@ -1,24 +1,16 @@
 import _ from 'lodash'
-// 引入 css 文件
-import './style.css'
-import Icon from './icon.jpg'
-import Data from './data.xml'
+import printMe from './print.js'
 
 function component() {
   var element = document.createElement('div');
+  var btn = document.createElement('button');
 
   // Lodash, currently included via a script, is required for this line to work
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  // 在元素上动态添加一个名叫 hello 的 class
-  element.classList.add('hello');
 
-  // 添加图片到已存在的 div 中
-  let image = new Image();
-  image.src = Icon;
-
-  element.appendChild(image);
-
-  console.log(Data);
+  btn.innerHTML = 'Click me and check the console!';
+  btn.onclick = printMe;
+  element.appendChild(btn);
 
   return element;
 }
--- webpack.config.js
+++ webpack.config.js
@@ -2,11 +2,14 @@ const path = require('path')
 
 module.exports = {
   // 文件入口
-  entry: './src/index.js',
+  entry: {
+    app: './src/index.js',
+    print: './src/print.js'
+  },
   // 出口配置
   output: {
     // 打包完成的文件名
-    filename: 'bundle.js',
+    filename: '[name].bundle.js',
     // 打包文件的输出路径
     path: path.resolve(__dirname, 'dist')
   }
