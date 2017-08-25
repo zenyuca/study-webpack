d4a5644a79cf943afd3e58fc5a9500232020cc19
zenyuca <zenyuca@163.com>
2017-08-26 00:31:03 +0800

    使用 clean-webpack-plugin 插件

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CleanWebpackPlugin = require('clean-webpack-plugin')
 
 module.exports = {
   // 文件入口
@@ -18,6 +19,8 @@ module.exports = {
     // 自动生成 html 文件，文件自动关联所有打包好的资源
     new HtmlWebpackPlugin({
       title: 'Output Management'
-    })
+    }),
+    // 每次构建时都先删除 dist 文件夹，然后再生成它。
+    new CleanWebpackPlugin(['dist'])
   ]
 }
