f3bf1949d04b0792dd872347508ca0805a8bdde6
zenyuca <zenyuca@163.com>
2017-08-27 23:41:19 +0800

    使用 Hot Module Replacement (HMR) 对项目进行热加载

--- src/print.js
+++ src/print.js
@@ -1,4 +1,5 @@
 export default function printMe() {
-  // console.log('I get called from print.js!');
-  cosnole.log('I get called from print.js!');
+  console.log('I get called from print.js!');
+  console.log('很好很好！');
+  // cosnole.log('I get called from print.js!');
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,14 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const CleanWebpackPlugin = require('clean-webpack-plugin')
+const webpack = require('webpack')
 
 module.exports = {
   // 文件入口
   entry: {
-    app: './src/index.js',
-    print: './src/print.js'
+    // app: './src/index.js',
+    // print: './src/print.js'
+    app: './src/index.js'
   },
   // 出口配置
   output: {
@@ -21,7 +23,8 @@ module.exports = {
   // devtool 选项详情地址 https://webpack.js.org/configuration/devtool/
   devtool: 'source-map',
   devServer: {
-    contentBase: './dist'
+    contentBase: './dist',
+    hot: true
   },
   plugins: [
     // 自动生成 html 文件，文件自动关联所有打包好的资源
@@ -29,6 +32,7 @@ module.exports = {
       title: 'Output Management'
     }),
     // 每次构建时都先删除 dist 文件夹，然后再生成它。
-    new CleanWebpackPlugin(['dist'])
+    new CleanWebpackPlugin(['dist']),
+    new webpack.HotModuleReplacementPlugin()
   ]
 }
