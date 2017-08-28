aff6fe3e57847e1d431ed08ea84c8d7c16aeca94
zenyuca <zenyuca@163.com>
2017-08-28 14:26:37 +0800

    使用 webpack.optimize.CommonsChunkPlugin 插件来分离公共的依赖，同时对其打包。

--- webpack.common.js
+++ webpack.common.js
@@ -1,15 +1,20 @@
 const path = require('path');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack')
 
 module.exports = {
   entry: {
-    app: './src/index.js'
+    app: './src/index.js',
+    another: './src/another-module.js'
   },
   plugins: [
     new CleanWebpackPlugin(['dist']),
     new HtmlWebpackPlugin({
       title: 'Production'
+    }),
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'common'
     })
   ],
   output: {
